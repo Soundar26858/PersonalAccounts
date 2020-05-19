4b0290e18119e7484ba5c2f56f7783872e3a70c6
Soundar <Soundarrjn@gmail.com>
2020-05-19 20:33:34 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalAccounts.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDesc { get; set; }
+        public string CategoryType { get; set; }
+
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalAccounts.Models
+{
+    public class Transaction
+    {
+        public int TransId { get; set; }
+        public double Amount { get; set; }
+        public DateTime TransDate { get; set; }
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
